28ab8fcbade7aa8833544cede5137fffbeccef97
Alon Shevach <67096125+alonShevach@users.noreply.github.com>
2021-05-17 02:45:31 +0300

    Delete build/lib/Pandas_Wrapper_pcg directory

--- build/lib/Pandas_Wrapper_pcg/constants.py
+++
@@ -1,7 +0,0 @@
-import pandas as pd
-
-DATA_1 = 'data/business-operations-survey-2020-covid-19-csv.csv'
-PEOPLE_DICT = {"weight": pd.Series([68, 83, 112], index=["alice", "bob", "charles"]),
-               "birthyear": pd.Series([1984, 1985, 1992], index=["bob", "alice", "charles"], name="year"),
-               "children": pd.Series([0, 3], index=["charles", "bob"]),
-               "hobby": pd.Series(["Biking", "Dancing"], index=["alice", "bob"]), }
