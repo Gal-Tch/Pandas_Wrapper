0743ec74e52c224477f3e6527b08dcf11a483325
alon.shevach <alon.shevach@mail.huji.ac.il>
2021-05-21 19:00:08 +0300

    fixed licenses

--- setup.py
+++ setup.py
@@ -5,24 +5,21 @@ with open("README.md", "r", encoding="utf-8") as fh:
 REQUIRED = ['bottle-websocket','pandas', 'numpy', 'eel']
 setuptools.setup(
     name="Pandas_Wrapper",
-    version="0.0.0",
-    author="Gal-Tch",
-    author_email="author@example.com", #???
+    version="0.0.1",
+    author="Alon Shevach",
+    author_email="alon.shevach1@gmail.com",
     description="A wrapper to the pandas package",
     install_requires=REQUIRED,
     long_description=long_description,
     long_description_content_type="text/markdown",
     url="https://github.com/Gal-Tch/Pandas_Wrapper.git",
-    project_urls={
-        "Bug Tracker": "https://github.com/pypa/sampleproject/issues",  # TODO: change this maybe
-    },
     classifiers=[
         "Programming Language :: Python :: 3",
-        "License :: OSI Approved :: MIT License",  # TODO: choose licence, also update licence file
+        "License :: OSI Approved :: BSD License",
         "Operating System :: OS Independent",
     ],
     py_modules=['Pandas_Wrapper_pcg'],
     package_dir={"": "src"},
     packages=setuptools.find_packages(where="src"),
-    python_requires=">=3.8",  # TODO: make sure its 3.8
+    python_requires=">=3.8",
 )
